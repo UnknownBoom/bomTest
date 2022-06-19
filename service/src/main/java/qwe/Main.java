d33e9576088fea845a438022085d0da6a8d606e6
artem <artem>
2022-06-19 14:09:36 +0300

    Initial commit

---
+++ service/src/main/java/qwe/Main.java
@@ -0,0 +1,10 @@
+package qwe;
+
+import com.google.common.collect.ImmutableMap;
+import org.jeasy.random.EasyRandom;
+
+public class Main {
+    public static void main(String[] args){
+        EasyRandom easyRandom = new EasyRandom();
+    }
+}
